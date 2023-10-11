2f8de73deefa2a256b60d7839d3293897f72ae1f
gajanangogurwar2022 <gajanangogurwar2022@gmail.com>
2023-10-11 16:58:06 +0530

    commited entityfrsmework data

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "INDIA",
+                    ShortName = "IND"
+                },
+                 new Country
+                 {
+                     Id = 2,
+                     Name = "PAKISTAN",
+                     ShortName = "PAK"
+                 },
+                  new Country
+                  {
+                      Id = 3,
+                      Name = "ENGLAND",
+                      ShortName = "ENG"
+                  }
+                );
+            //modelBuilder.Entity<Hotel>().HasData(
+            //    new Hotel
+            //    {
+            //        Id = 1,
+            //        Name = "Jameka",
+            //        Address = "Mukhed",
+            //        CountryId = 1,
+            //        Rating = 1.1
+            //    },
+            //     new Hotel
+            //     {
+            //         Id = 2,
+            //         Name = "Jameka2",
+            //         Address = "Mukhed2",
+            //         CountryId = 2,
+            //         Rating = 1.1
+            //     },
+            //     new Hotel
+            //     {
+            //         Id = 4,
+            //         Name = "Jameka4",
+            //         Address = "Mukhed4",
+            //         CountryId = 3,
+            //         Rating = 4.1
+            //     },
+            //      new Hotel
+            //      {
+            //          Id = 3,
+            //          Name = "Jameka3",
+            //          Address = "Mukhed3",
+            //          CountryId = 3,
+            //          Rating = 3.2
+            //      }
+            //    );
+
+        }
+    }
+}
---
+++ HotelListing.API/Migrations/20231011111813_data.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Mukhed", 1, "Jameka", 1.1000000000000001 },
+                    { 2, "Mukhed2", 2, "Jameka2", 1.1000000000000001 },
+                    { 3, "Mukhed3", 3, "Jameka3", 3.2000000000000002 },
+                    { 4, "Mukhed4", 3, "Jameka4", 4.0999999999999996 }
+                });
+        }
+    }
+}
